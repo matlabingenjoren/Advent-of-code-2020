58b13438e3079985b5e1511a745de4b56c7190ed
Oskar Ekberg <75374438+matlabingenjoren@users.noreply.github.com>
2020-12-03 15:06:04 +0100

    Add files via upload

--- dec_3/dec_3.py
+++ dec_3/dec_3.py
@@ -1,34 +1,32 @@
-
 f = open("C:/Users/Oskar/Documents/Python/24 days of code/dec_3/dec_3.txt", "r")
-list = f.readlines()
-
-slope = [i.replace('\n','') for i in list] 
+temp = f.readlines()
+hill = [i.replace('\n','') for i in temp] 
 
 #Q1
-# u=0
+# vert=0
 # counter=0
-# for i in range(len(slope)):
-#     u = u % 31
-#     if slope[i][u] == '#':
+# for hor horn range(len(hill)):
+#     vert = vert % 31
+#     if hill[hor][vert] == '#':
 #         counter += 1
-#     u += 3
+#     vert += 3
 # print(counter)
 
 
 #Q2
-case = [[1,1],[3,1],[5,1],[7,1],[1,2],]     #[rightstep,downstep]
+slopes = [[1,1],[3,1],[5,1],[7,1],[1,2],]     #[rightstep,downstep]
 trees=1
-for a in range(len(case)):
+for case in range(len(slopes)):
     counter = 0
-    i=0
-    u=0
+    hor=0
+    vert=0
     while True:
-        u = u % 31
-        if slope[i][u] == '#':
+        vert = vert % 31
+        if hill[hor][vert] == '#':
             counter += 1
-        u += case[a][0]
-        i += case[a][1]
-        if i >= len(slope):
+        vert += slopes[case][0]
+        hor += slopes[case][1]
+        if hor >= len(hill):
             trees=trees*counter
             break
 print(trees)
